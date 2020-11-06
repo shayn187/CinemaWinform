404974a1259336d882024a20dcf8d01565a09d8f
NganHua <NganHTCE140552@fpt.edu.vn>
2020-11-06 14:20:08 +0700

    fullscreen

--- CinemaManagement/Form1.cs
+++ CinemaManagement/Form1.cs
@@ -24,10 +24,26 @@ namespace CinemaManagement
             //skinManager.AddFormToManage(this);
             skinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
             skinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Red300, MaterialSkin.Primary.Red900, MaterialSkin.Primary.Blue500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+
+            //pnlLogin.Left = (this.Width - pnlLogin.Width) / 2;
+            //pnlLogin.Top = (this.Height - pnlLogin.Height) / 2;
+            pnlLogin.Location = new Point((this.Width - pnlLogin.Width) / 2, (this.Height - pnlLogin.Height) / 2);
+
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+        private void CenterControlInParent(Control ctrlToCenter)
+        {
+            ctrlToCenter.Left = (ctrlToCenter.Parent.Width - ctrlToCenter.Width) / 2;
+            ctrlToCenter.Top = (ctrlToCenter.Parent.Height - ctrlToCenter.Height) / 2;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            CenterControlInParent(pnlLogin);
         }
+
     }
+
 }
